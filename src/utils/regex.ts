cf5f7005b160cd483424b0ee7e4b2f544c5226d1
WeslleySOR <weslleywruas@gmail.com>
2022-04-09 19:35:14 -0300

    fix: bug in the regex where it doesnt accept to capitalize the plate number

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,3 +1,3 @@
 export function regexToValidatePlateNumber(text: string) {
-	return /^([a-z]{3}-[0-9]{4})$/.test(text); // Se o texto for aaa-0000 por exemplo, retornará true
+	return /^([A-Za-z]{3}-[0-9]{4})$/.test(text); // Se o texto for aaa-0000 por exemplo, retornará true
 }
